fa495daf0665bb5a584794822595ea857bddebff
besufkad-ayele <ayebesufkad@gmail.com>
2024-07-29 14:53:32 +0300

    its what we expect to work new

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,11 +4,23 @@ import requests
 import sys
 
 if __name__ == "__main__":
+    
     url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
-    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
-
-    completed = [t.get("title") for t in todos if t.get("completed") is True]
-    print("Employee {} is done with tasks({}/{}):".format(
-        user.get("name"), len(completed), len(todos)))
-    [print("\t {}".format(c)) for c in completed]
+    employee_id = sys.argv[1]
+    user_response = requests.get(url + "users/{}".format(employee_id))
+    user = user_response.json()
+    # user = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    params = {"userId": employee_id}
+    todos_response = requests.get(url + "todos", params=params)
+    todos = todos_response.json()
+    completed = []
+    for todo in todos:
+        if todo.get("completed") is True:
+            completed.append(todo.get("title"))
+            
+    print("Employee {} is done with tasks({}/{}):".format(user.get("name"),
+                                                            len(completed),
+                                                            len(todos)))
+    for task in completed:
+        print("\t {}".format(task))
+    
