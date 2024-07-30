0294501ae44e8064e635e35e10f26109fe1d7825
besufkad-ayele <ayebesufkad@gmail.com>
2024-07-30 09:03:30 +0300

    these is for last in these file

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -10,15 +10,13 @@ def fetch_user_data():
     url = "https://jsonplaceholder.typicode.com/"
     users = requests.get(url + "users").json()
     data_to_export = {}
-    
-    for user in users:
+        for user in users:
         user_id = user.get("id")
         todos_response = requests.get(
             url + "todos", params={"userId": user_id}
         )
         todo_list = todos_response.json()
         data_to_export[user_id] = []
-        
         for todo in todo_list:
             task_info = {
                 "task": todo.get("title"),
@@ -28,7 +26,6 @@ def fetch_user_data():
             data_to_export[user_id].append(task_info)
     
     return data_to_export
-
 
 if __name__ == "__main__":
     data_to_export = fetch_user_data()
